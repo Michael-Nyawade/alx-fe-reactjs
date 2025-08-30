31b3f20e08a70c178465389d16957c259e7df80b
Michael-Nyawade <mikeokello17@gmail.com>
2025-08-30 18:29:50 +0300

    Created Header, Main Content, and Footer components in the React App.

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,7 +2,11 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+
 import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./Header";
+import MainContent from "./MainContent";
+import Footer from "./Footer";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -18,8 +22,10 @@ function App() {
         </a>
       </div>
 
-      {/* Inserted our custom JSX component */}
-      <WelcomeMessage />
+      {/* Our custom components */}
+      <Header />
+      <MainContent />
+      <Footer />
 
       <h1>Vite + React</h1>
       <div className="card">
