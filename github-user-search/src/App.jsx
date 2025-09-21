33b86b1bb94b86855eaf7d6f9165f4e4b4448830
Michael-Nyawade <mikeokello17@gmail.com>
2025-09-21 16:57:47 +0300

    Integrated the GitHub API and Created a Basic Search Feature.

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,22 +1,10 @@
-import { useState } from "react";
-import SearchBar from "./components/SearchBar";
+import Search from "./components/Search";
 
 function App() {
-  const [user, setUser] = useState(null);
-
   return (
     <div style={{ padding: "2rem", textAlign: "center" }}>
       <h1>GitHub User Search</h1>
-      <SearchBar setUser={setUser} />
-      {user && (
-        <div style={{ marginTop: "2rem" }}>
-          <img src={user.avatar_url} alt={user.login} width={100} />
-          <h2>{user.login}</h2>
-          <a href={user.html_url} target="_blank" rel="noreferrer">
-            View Profile
-          </a>
-        </div>
-      )}
+      <Search />
     </div>
   );
 }
